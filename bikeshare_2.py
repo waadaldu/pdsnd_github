b65c393625f3d6af50fc388f7cc286f269a400b6
“waad <waadaldu@gmail.com>
2021-09-20 22:47:02 +0300

    date and day added as a comment to the code

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,6 +2,8 @@ import time
 import pandas as pd
 import numpy as np
 
+# date; 19.september.21 
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york': 'new_york_city.csv',
               'washington': 'washington.csv' }
