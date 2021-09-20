00e22330ccfebf0407ecbcb219699f99f03ee7b1
“waad <waadaldu@gmail.com>
2021-09-20 22:30:41 +0300

    comment added to bikeshare_2 code

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -2,6 +2,9 @@ import time
 import pandas as pd
 import numpy as np
 
+# beggining of method one 
+
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
